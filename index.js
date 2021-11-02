fa08c3b1865bde892cad92963833f5feb428400f
xcript1 <o_olatunji@mail.harper.edu>
2021-11-02 11:48:41 -0500

    finished CRUD implementation for an express app.

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require("express")
 const app = express()
 const path = require('path')
+const methodOverride = require('method-override')
 const { v4: uuid } = require("uuid")
 uuid()
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(methodOverride('_method'))
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 
-const comments = [
+let comments = [
     {
         id: uuid(),
         username: 'Todd',
@@ -52,6 +54,23 @@ app.get('/comments/:id', (req, res) => {
     const comment = comments.find(c => c.id === id)
     res.render('comments/show', {comment})
 }) 
+app.patch('/comments/:id', (req, res) => {
+    const { id } = req.params
+    const newCommentText = req.body.comment
+    const foundComment = comments.find(c => c.id === id)
+    foundComment.comment = newCommentText
+    res.redirect('/comments')
+})
+app.get('/comments/:id/edit', (req, res) => {
+    const {id} = req.params
+    const comment = comments.find(c => c.id === id)
+    res.render('comments/edit', { comment })
+})
+app.delete('/comments/:id', (req, res) => {
+    const {id} = req.params
+    comments = comments.filter(c => c.id !== id);
+    res.redirect('/comments')
+})
 
 // app.get('/tacos', (req, res) => {
 //     res.send("GET /tacos response")
